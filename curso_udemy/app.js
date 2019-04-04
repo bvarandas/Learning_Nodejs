3230edddcc6591fe92e96f342272579c053b9de3
Bruno Varandas Ribeiro <b_varandas@LIVE.COM>
2019-04-04 17:29:57 -0300

    aulas de nodejs até a 18

---
+++ curso_udemy/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app     = express();
+var msg = require("./mod_teste");
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('home/index');
+});
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render('admin/form_add_noticia');
+});
+
+app.get('/noticias', function(req, res){
+    res.render('noticias/noticias');
+});
+
+app.listen(3000, function(req, res)
+    {
+        console.log(msg());
+        console.log("servidor rodando com express");
+    });
--- curso_udemy/noticias.js
+++ curso_udemy/noticias.js
@@ -1,1 +1,24 @@
-console.log('Criando um site de notiias com nodejs.')
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    var categoria = req.url;
+    if (categoria == '/tecnologia')
+    {
+        res.end('<html><body>Noticias de tecnologia</body></html>')
+
+    }else if (categoria == '/moda')
+    {
+        res.end('<html><body>Noticias de moda</body></html>')
+    }
+    else if (categoria == '/beleza')
+    {
+        res.end('<html><body>Noticias de beleza</body></html>')
+    }else
+    {
+        res.end("<html><body>Portal de notícias</body></html>");
+    }
+    
+        
+});
+
+server.listen(3000);
